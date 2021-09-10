a6d0c8b7f4adbbd75ca9e6e7de57f6762e3bd68e
DivOpsUS <admin@divops.us>
2021-09-09 21:56:59 -0700

    9:57PM

--- src/utils/Modal.js
+++ src/utils/Modal.js
@@ -1,15 +1,8 @@
-import React, { useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
-import Transition from '../utils/Transition.js';
+import React, { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import Transition from "../utils/Transition.js";
 
-function Modal({
-  children,
-  id,
-  ariaLabel,
-  show,
-  handleClose
-}) {
-
+function Modal({ children, id, ariaLabel, show, handleClose }) {
   const modalContent = useRef(null);
 
   // close the modal on click outside
@@ -18,9 +11,9 @@ function Modal({
       if (!show || modalContent.current.contains(target)) return;
       handleClose();
     };
-    document.addEventListener('click', clickHandler);
-    return () => document.removeEventListener('click', clickHandler);
-  });  
+    document.addEventListener("click", clickHandler);
+    return () => document.removeEventListener("click", clickHandler);
+  });
 
   // close the modal if the esc key is pressed
   useEffect(() => {
@@ -28,45 +21,48 @@ function Modal({
       if (keyCode !== 27) return;
       handleClose();
     };
-    document.addEventListener('keydown', keyHandler);
+    document.addEventListener("keydown", keyHandler);
 
-    return () => document.removeEventListener('keydown', keyHandler);
-  });  
+    return () => document.removeEventListener("keydown", keyHandler);
+  });
 
   return (
     <>
       {/* Modal backdrop */}
       <Transition
-        className="fixed inset-0 z-50 bg-white bg-opacity-75 transition-opacity blur"
+        className='fixed inset-0 z-50 bg-white bg-opacity-75 transition-opacity blur'
         show={show}
-        enter="transition ease-out duration-200"
-        enterStart="opacity-0"
-        enterEnd="opacity-100"
-        leave="transition ease-out duration-100"
-        leaveStart="opacity-100"
-        leaveEnd="opacity-0"
-        aria-hidden="true"
+        enter='transition ease-out duration-200'
+        enterStart='opacity-0'
+        enterEnd='opacity-100'
+        leave='transition ease-out duration-100'
+        leaveStart='opacity-100'
+        leaveEnd='opacity-0'
+        aria-hidden='true'
       />
 
       {/* Modal dialog */}
       <Transition
         id={id}
-        className="fixed inset-0 z-50 overflow-hidden flex items-center justify-center transform px-4 sm:px-6"
-        role="dialog"
-        aria-modal="true"
+        className='fixed inset-0 z-50 overflow-hidden flex items-center justify-center transform px-4 sm:px-6'
+        role='dialog'
+        aria-modal='true'
         aria-labelledby={ariaLabel}
         show={show}
-        enter="transition ease-out duration-200"
-        enterStart="opacity-0 scale-95"
-        enterEnd="opacity-100 scale-100"
-        leave="transition ease-out duration-200"
-        leaveStart="opacity-100 scale-100"
-        leaveEnd="opacity-0 scale-95"
+        enter='transition ease-out duration-200'
+        enterStart='opacity-0 scale-95'
+        enterEnd='opacity-100 scale-100'
+        leave='transition ease-out duration-200'
+        leaveStart='opacity-100 scale-100'
+        leaveEnd='opacity-0 scale-95'
       >
-        <div className="bg-white overflow-auto max-w-6xl w-full max-h-full" ref={modalContent}>          
+        <div
+          className='bg-white overflow-auto max-w-6xl w-full max-h-full'
+          ref={modalContent}
+        >
           {children}
         </div>
-      </Transition>    
+      </Transition>
     </>
   );
 }
@@ -76,10 +72,10 @@ export default Modal;
 Modal.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.element),
-    PropTypes.element.isRequired
+    PropTypes.element.isRequired,
   ]),
   id: PropTypes.string,
   ariaLabel: PropTypes.string,
   show: PropTypes.bool.isRequired,
-  handleClose: PropTypes.func.isRequired
+  handleClose: PropTypes.func.isRequired,
 };
