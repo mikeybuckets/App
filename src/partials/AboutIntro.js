ced276a9b7c7f5b4303915742e36ed23f6b44b6a
DivOpsUS <admin@divops.us>
2021-09-09 17:04:15 -0700

    hero

--- src/partials/AboutIntro.js
+++ src/partials/AboutIntro.js
@@ -8,34 +8,36 @@ function AboutIntro() {
           {/* Section header */}
           <div className='max-w-3xl mx-auto text-center pb-12 md:pb-16'>
             <h1
-              className='text-white text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4'
+              className='text-white text-2xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4'
               data-aos='zoom-y-out'
             >
-              Get to know Mike{" "}
+              {" "}
               <span className='bg-clip-text text-transparent bg-gradient-to-r from-gray-200 to-gray-700'>
                 I was born in Mesa Az
               </span>
             </h1>
-            <div className='flex row title-container justify-center'>
-              <h5 className='relative xt-xl text-gray-600'>
-                I was born in Mesa Az...
-                <img
-                  className='rounded shadow-2xl item-center mxy-auto'
-                  src={require("../images/sun-fog-43.png").default}
-                  alt='About us'
-                />
+            <div>
+              <img
+                className='rounded shadow-2xl item-center mxy-auto'
+                src={require("../images/sun-fog-43.png").default}
+                alt='About us'
+              />
+            </div>
+
+            <figure className='flex justify-center items-start'>
+              <img
+                className='rounded shadow-2xl mxy-auto'
+                src={require("../images/the-family.jpg").default}
+                alt='About us'
+              />
+            </figure>
+
+            <div className='max-w-3xl mx-auto text-center pb-12 md:pb-16'>
+              <span className='bg-clip-text text-5xl text-gray-100 hover:text-white'>
                 I come from a great family!
-              </h5>
+              </span>
             </div>
           </div>
-
-          <figure className='flex justify-center items-start'>
-            <img
-              className='rounded shadow-2xl mxy-auto'
-              src={require("../images/the-family.jpg").default}
-              alt='About us'
-            />
-          </figure>
         </div>
       </div>
     </section>
