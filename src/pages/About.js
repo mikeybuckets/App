b71c9c326483b71a4dbcc6f17df9ac80ae8e0634
DivOpsUS <admin@divops.us>
2021-09-09 20:11:48 -0700

    8:11PM

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,39 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../partials/Header';
-import AboutIntro from '../partials/AboutIntro';
-import AboutStory from '../partials/AboutStory';
-import Stats from '../partials/Stats';
-import Team from '../partials/Team';
-import Career from '../partials/Career';
-import Process from '../partials/Process';
-import CtaAlternative from '../partials/CtaAlternative';
-import Footer from '../partials/Footer';
+import Header from "../partials/Header";
+import AboutIntro from "../partials/AboutIntro";
+import Stats from "../partials/Stats";
+import Team from "../partials/Team";
+import Career from "../partials/Career";
+import Process from "../partials/Process";
+import CtaAlternative from "../partials/CtaAlternative";
+import Footer from "../partials/Footer";
 
 function About() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-
+    <div className='flex flex-col min-h-screen overflow-hidden'>
       {/*  Site header */}
       <Header />
 
       {/*  Page content */}
-      <main className="flex-grow">
-
+      <main className='flex-grow'>
         {/*  Page sections */}
         <AboutIntro />
-        <AboutStory />
         <Stats />
         <Team />
         <Career />
         <Process />
         <CtaAlternative />
-
       </main>
 
       {/*  Site footer */}
       <Footer />
-
     </div>
   );
 }
--- src/partials/AboutIntro.js
+++ src/partials/AboutIntro.js
@@ -4,38 +4,32 @@ function AboutIntro() {
   return (
     <section className='bg-gray-900'>
       <div className='max-w-6xl mx-auto px-4 sm:px-6 bg-gray-900'>
-        <div className='pt-32 pb-12 md:pt-40 md:pb-20'>
+        <div className='pt-16 pb-12 md:pt-15 md:pb-10'>
           {/* Section header */}
-          <div className='max-w-3xl mx-auto text-center pb-12 md:pb-16'>
+          <div className='max-w-3xl mx-auto text-center'>
             <h1
-              className='text-white text-2xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4'
+              className='text-white text-6xl mx-auto md:text-6xl md:mx-10 font-extrabold leading-tighter tracking-tighter'
               data-aos='zoom-y-out'
             >
-              {" "}
-              <span className='bg-clip-text text-transparent bg-gradient-to-r from-gray-200 to-gray-700'>
-                I was born in Mesa Az
-              </span>
+              <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-gray-200 to-gray-700 myx-auto'>
+                The Valley
+              </h1>
+              <span>Arizona</span>
             </h1>
             <div>
               <img
-                className='rounded shadow-2xl item-center mxy-auto'
+                className='rounded shadow-2xl item-center my-10 py-0 px-12 mxy-auto'
                 src={require("../images/sun-fog-43.png").default}
                 alt='About us'
               />
             </div>
 
-            <figure className='flex justify-center items-start'>
+            <div className='flex justify-center items-start'>
               <img
                 className='rounded shadow-2xl mxy-auto'
                 src={require("../images/the-family.jpg").default}
                 alt='About us'
               />
-            </figure>
-
-            <div className='max-w-3xl mx-auto text-center pb-12 md:pb-16'>
-              <span className='bg-clip-text text-5xl text-gray-100 hover:text-white'>
-                I come from a great family!
-              </span>
             </div>
           </div>
         </div>
